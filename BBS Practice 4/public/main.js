12ed3e69a67bc3b5aaa0184b91b469457d001aef
Jihyo Kang <luxuryjihyo@naver.com>
2022-03-12 11:52:32 +0900

    12 Mar 2022

---
+++ BBS Practice 4/public/main.js
@@ -0,0 +1,31 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD1U6-b7LBLBOynzrU085qc6tZJwbxZ1U8",
+  authDomain: "up-project-bb641.firebaseapp.com",
+  projectId: "up-project-bb641",
+  storageBucket: "up-project-bb641.appspot.com",
+  messagingSenderId: "911219651262",
+  appId: "1:911219651262:web:113fded73357fac23564ef"
+};
+
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+const storage = firebase.storage();
+let store = {
+  currentPage : 1,
+}
+
+function getCurrentTime(val){
+  let _t = val
+  let yyyy = _t.getFullYear()
+  let mm = _t.getMonth() < 10 ? '0' + _t.getMonth() : _t.getMonth()
+  let dd = _t.getDate() < 10 ? '0' + _t.getDate() : _t.getDate()
+  let hh = _t.getHours()  < 10 ? '0' + _t.getHours() : _t.getHours()
+  let min = _t.getMinutes()  < 10 ? '0' + _t.getMinutes() : _t.getMinutes()
+  let sec = _t.getSeconds()  < 10 ? '0' + _t.getSeconds() : _t.getSeconds()
+  return `${yyyy}/${mm}/${dd} ${hh}:${min}:${sec}` 
+}
+
+
+
